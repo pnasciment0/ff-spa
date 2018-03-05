1aeded4fda2fca37817f6d1e85e79ace563453e6
Paulo Nascimento <pnascimento@uchicago.edu>
2018-03-04 23:35:12 -0600

    firebase added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <SearchBar/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchBar, HelloWorld
   }
 }
 </script>
@@ -23,6 +25,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-bottom: 100px;
+}
+html {
+  background: linear-gradient(#d442f4, #6b1d7c);
+  background-repeat: no-repeat;
+  min-height: 786px;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search-bar-wrapper">
+        <input type="text" class="search-bar">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchBar"
+    }
+</script>
+
+<style scoped>
+    .search-bar {
+        width: 80%;
+        height: 60px;
+        background: transparent;
+    }
+    input[type=text] {
+        font-size: 45px;
+    }
+    input:focus {
+        /*border: none;*/
+        border-bottom: 2px;
+        outline: none;
+    }
+    input {
+        /*border: none;*/
+        border-bottom: 2px;
+        outline: none;
+    }
+</style>
