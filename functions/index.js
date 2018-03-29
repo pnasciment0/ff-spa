ff6fd3ed0f780988af2fb79b085344c7adbb2175
Paulo Nascimento <pnascimento@uchicago.edu>
2018-03-29 16:50:48 -0500

    we firebase functions now

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const request = require('request');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,19 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+const app = express();
+
+app.use(cors({
+    origin: true
+}));
+app.get("/test", (req, res) => {
+    res.send("test test test");
+});
+
+const api = functions.https.onRequest(app);
+
+module.exports = {
+    api
+};
